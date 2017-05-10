6bac16764b953f39e82ef5dc4079e3e245e7f2d5
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2017-05-09 23:59:15 -0400

    Removed testart endpoint

--- server.js
+++ server.js
@@ -142,18 +142,3 @@ app.get("/getart",function(req, res){
     });
   res.end("response");
 });
-
-app.get("/testart",function(req, res){
-  var pieces = [{title: "Arosa I Placard", url: "http://images.complex.com/complex/image/upload/c_limit,w_680/fl_lossy,pg_1,q_auto/o1sliwrtuiji7yqowzjj.jpg", artistID: "a2", countryOfOrigin: "Switzerland", year: 1936, medal: "gold", gamesCountry: "Berlin, Germany"},
-                {title: "Rugby", url: "https://upload.wikimedia.org/wikipedia/commons/6/61/Rugby_by_Jean_Jacoby.png", artistID: "a1", countryOfOrigin: "Luxembourg", year: 1928, medal: "gold", gamesCountry: "Berlin, Germany"},
-                //{title: "", url: "", artistID: "", countryOfOrigin: "", year: , medal: ""}
-              ]
-  var artists = {'a1':{name: "Jean Jacoby",
-                       bio:"Jean Lucien Nicolas Jacoby was a Luxembourg artist. He won Olympic gold medals in the Olympic art competitions of 1924 and 1928, making him the most successful Olympic artist ever.",
-                       location: "New York City"},
-                 'a2':{name: "Alex Diggelmann",
-                       bio: "Alex Walter Diggelmann was a Swiss graphic artist and book designer best known for his sports posters.[1] Diggelmann who won three medals in the Olympic Games.",
-                       location: "Switzerland"}
-                     };
-  res.end(JSON.stringify({'pieces': pieces, 'artists': artists}));
-});
